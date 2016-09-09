f6a3fa78ec8787b2a3a0de0844a3750221e20cc6
Joe Delgado <jdelgado@flatiron.com>
2016-09-09 01:44:45 -0400

    Updating tests

--- tests/index.js
+++ tests/index.js
@@ -1,15 +1,53 @@
 import { expect } from 'chai';
-import { sum } from '../src/index.js';
+import { ImmutableReducer } from '../src/index';
+import { createReducer } from 'redux-caller';
+import _ from 'underscore';
+
 const { describe, it } = global;
 
-describe('sum', () => {
-  it('should add two numbers correctly', () => {
-    const result = sum(10, 20);
-    expect(result).to.be.equal(30);
+const ACTIONS = {
+  SET_COUNT: 'SET_COUNT'
+};
+
+class MockStore extends ImmutableReducer {
+  [ACTIONS.SET_COUNT]({payload}) {
+    return this.set('count', payload);
+  }
+}
+
+MockStore.defaultProperties = {
+  count: 1
+};
+
+describe('createReducer', () => {
+  it('should handle undefined initialization', () => {
+    const reducer = createReducer(new MockStore());
+
+    expect(reducer(undefined, {type: 'INIT', payload: null}).count)
+        .to.be.equal(1);
   });
 
-  it('should return the same number if second param is null', () => {
-    const result = sum(10, null);
-    expect(result).to.be.equal(10);
+  it('should handle actions', () => {
+    const reducer = createReducer(new MockStore());
+
+    expect(reducer(undefined, {type: ACTIONS.SET_COUNT, payload: 5}).count)
+        .to.be.equal(5);
   });
+
+  it('should be callable multiple times', () => {
+    const reducer = createReducer(new MockStore());
+
+    let states = [];
+    _.range(20).reduce((state, i) => {
+      const nextState = reducer(state, {type: ACTIONS.SET_COUNT, payload: i});
+      states.push(nextState);
+      return nextState;
+    }, undefined);
+
+    _(states).each((state, i) => {
+      expect(state).to.be.an.instanceof(MockStore);
+      expect(state.toJS().count).to.be.equal(i);
+    });
+  });
+
 });
